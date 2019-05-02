f8c79ff157c38ee4620bcb9e3d6b089250fd6bfa
cletsxdv <jclementehdzhdz@gmail.com>
2019-05-01 22:06:11 -0500

    algoritmo en comentarios

---
+++ src/main_naive_bayes.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // cargo run --bin main_naive_bayes.rs
+
+    // Crear matriz de datos para el training
+    // Crear vector para las clases
+        // - Este vector contiene las clases por las que se pueden clasificar las entradas
+
+    // -> Training
+    // Calcular la probabilidad a priori de cada clase
+        // Crear un vector con la misma cantidad de clases
+        // Calcular la probabilidad y almacenarla en la casilla correspondiente del vector
+
+    // Calcular frecuencia de valores de atributos por clase
+        // Calcular la corrección de Laplace
+    // Calcular la normalización de las frecuencias de cada valor de los atributos
+
+    // Testing
+    // Dada una entrada, calcular todas las probabilidades
+    // Calcular cuál es la mejor probabilidad
+}
